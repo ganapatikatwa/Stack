b1769501d0d82b1d3b138d3a42ba09f95a8af442
Ganapati <ganapatasakatwa@gmail.com>
2023-06-03 05:19:46 +0530

    [GK] Create Stack using Linked List

---
+++ Stack/Program.cs
@@ -0,0 +1,19 @@
+﻿using Stack;
+using System;
+namespace LinkedList
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            StackEx ex = new StackEx();
+            ex.Push(70);
+            ex.Push(30);
+            ex.Push(56);
+            ex.Display();
+            //Console.WriteLine("\n\n");
+            //ex.Pop();
+            //ex.Display();       
+        }
+    }
+}
---
+++ Stack/StackEx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    public class StackEx
+    {
+        Node top;
+        public StackEx()
+        {
+            top = null;
+        }
+        public void Push(int data)
+        {
+            Node newNode = new Node(data);
+            newNode.Next = top;
+            top = newNode;
+        }
+        public void Display()
+        {
+            if(top==null)
+            {
+                Console.WriteLine("Stack is Empty");
+            }
+            else
+            {
+                Node temp=top;
+                while(temp!=null)
+                {
+                    Console.WriteLine("|"+ temp.Data+"|");
+                    Console.WriteLine("__________");
+                    temp= temp.Next;
+                }
+            }
+        }
+        public void Pop()
+        {
+            if(top==null)
+            {
+                Console.WriteLine("Stack is Empty");
+            }
+            else
+            {
+                top= top.Next;
+            }
+        }
+
+    }
+}
