378af3c38fd72ea48ec2fb070bde5e0a2b208cdb
Ganapati <ganapatasakatwa@gmail.com>
2023-06-03 06:04:06 +0530

    [GK] Adding in Queue

--- Stack/Program.cs
+++ Stack/Program.cs
@@ -1,24 +1,50 @@
 ﻿using Stack;
 using System;
+using System.Collections.Generic;
+
 namespace LinkedList
 {
     public class Program
     {
         public static void Main(String[] args)
         {
-            StackEx ex = new StackEx();
-            ex.Push(70);
-            ex.Push(30);
-            ex.Push(56);
-            ex.Display();
-            Console.WriteLine("\n\n");
-            //ex.Pop();
-            //ex.Pop();
-            //ex.Pop();
-            //ex.Display();
-            ex.Peek();
-            ex.Display();
-
+            while (true)
+            {
+                Console.WriteLine("Enter the Choice\n1)Stack\n2)Pop and Peek\n3)Enqueue");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        StackEx ex = new StackEx();
+                        ex.Push(70);
+                        ex.Push(30);
+                        ex.Push(56);
+                        ex.Display();
+                        break;
+                    case 2:
+                        //StackEx ex = new StackEx();
+                        //ex.Push(70);
+                        //ex.Push(30);
+                        //ex.Push(56);
+                        //ex.Display();
+                        //ex.Pop();
+                        //ex.Pop();
+                        //ex.Pop();
+                        //ex.Display();
+                        //ex.Peek();
+                        //ex.Display();
+                        //break;
+                    case 3:
+                        QueueEx qx =new QueueEx();
+                        qx.Enqueue(56);
+                        qx.Enqueue(30);
+                        qx.Enqueue(70);
+                        qx.DisplayQueue();
+                        break;
+                }
+               
+            
+            }
         }
     }
 }
---
+++ Stack/QueueEx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    public class QueueEx
+    {
+        Node front;
+        Node rear;
+        public void Enqueue(int data)
+        {
+            Node newnode = new Node(data);
+            if (front == null)
+            {
+                rear = newnode;
+                front = rear;
+            }
+            else
+            {
+                rear.Next = newnode;
+                rear= rear.Next;
+            }
+        }
+        public void DisplayQueue()
+        {
+            
+            if(front==null)
+            {
+                Console.WriteLine("Queue is Empty");
+            }
+            Node temp = front;
+                while(temp!=null)
+                {
+                    Console.WriteLine(" | "+temp.Data+" | ");
+                    temp=temp.Next;
+                }
+        }
+    }
+}
